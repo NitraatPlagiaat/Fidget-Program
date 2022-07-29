10796a110fa8af2f4ea23a0b8f51268456ecadd2
NitraatPlagiaat <yirnick@live.nl>
2022-07-29 11:57:06 +0200

    Basics fidget program

--- Fidget-Program/Form1.Designer.cs
+++ Fidget-Program/Form1.Designer.cs
@@ -29,13 +29,138 @@ namespace Fidget_Program
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_off = new System.Windows.Forms.Button();
+            this.btn_on = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tb_fidget = new System.Windows.Forms.TextBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btn_rammstein = new System.Windows.Forms.Button();
+            this.btn_slipknot = new System.Windows.Forms.Button();
+            this.btn_cannibalCorpse = new System.Windows.Forms.Button();
+            this.btn_behemoth = new System.Windows.Forms.Button();
+            this.btn_muse = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_off);
+            this.groupBox1.Controls.Add(this.btn_on);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // btn_off
+            // 
+            resources.ApplyResources(this.btn_off, "btn_off");
+            this.btn_off.Name = "btn_off";
+            this.btn_off.UseVisualStyleBackColor = true;
+            this.btn_off.Click += new System.EventHandler(this.btn_off_Click);
+            // 
+            // btn_on
+            // 
+            resources.ApplyResources(this.btn_on, "btn_on");
+            this.btn_on.Name = "btn_on";
+            this.btn_on.UseVisualStyleBackColor = true;
+            this.btn_on.Click += new System.EventHandler(this.btn_on_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tb_fidget);
+            this.groupBox2.Controls.Add(this.btn_clear);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // tb_fidget
+            // 
+            resources.ApplyResources(this.tb_fidget, "tb_fidget");
+            this.tb_fidget.Name = "tb_fidget";
+            // 
+            // btn_clear
+            // 
+            resources.ApplyResources(this.btn_clear, "btn_clear");
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btn_muse);
+            this.groupBox3.Controls.Add(this.btn_behemoth);
+            this.groupBox3.Controls.Add(this.btn_cannibalCorpse);
+            this.groupBox3.Controls.Add(this.btn_slipknot);
+            this.groupBox3.Controls.Add(this.btn_rammstein);
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // btn_rammstein
+            // 
+            resources.ApplyResources(this.btn_rammstein, "btn_rammstein");
+            this.btn_rammstein.Name = "btn_rammstein";
+            this.btn_rammstein.UseVisualStyleBackColor = true;
+            this.btn_rammstein.Click += new System.EventHandler(this.btn_rammstein_Click);
+            // 
+            // btn_slipknot
+            // 
+            resources.ApplyResources(this.btn_slipknot, "btn_slipknot");
+            this.btn_slipknot.Name = "btn_slipknot";
+            this.btn_slipknot.UseVisualStyleBackColor = true;
+            // 
+            // btn_cannibalCorpse
+            // 
+            resources.ApplyResources(this.btn_cannibalCorpse, "btn_cannibalCorpse");
+            this.btn_cannibalCorpse.Name = "btn_cannibalCorpse";
+            this.btn_cannibalCorpse.UseVisualStyleBackColor = true;
+            // 
+            // btn_behemoth
+            // 
+            resources.ApplyResources(this.btn_behemoth, "btn_behemoth");
+            this.btn_behemoth.Name = "btn_behemoth";
+            this.btn_behemoth.UseVisualStyleBackColor = true;
+            // 
+            // btn_muse
+            // 
+            resources.ApplyResources(this.btn_muse, "btn_muse");
+            this.btn_muse.Name = "btn_muse";
+            this.btn_muse.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_off;
+        private System.Windows.Forms.Button btn_on;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox tb_fidget;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btn_muse;
+        private System.Windows.Forms.Button btn_behemoth;
+        private System.Windows.Forms.Button btn_cannibalCorpse;
+        private System.Windows.Forms.Button btn_slipknot;
+        private System.Windows.Forms.Button btn_rammstein;
     }
 }
 
--- Fidget-Program/Form1.cs
+++ Fidget-Program/Form1.cs
@@ -16,5 +16,37 @@ namespace Fidget_Program
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Enable and disable buttons
+        /// </summary>
+        /// 
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_on_Click(object sender, EventArgs e)
+        {
+            btn_off.Enabled = true;
+            btn_on.Enabled = false;
+        }
+        /// <summary>
+        /// Enable and disable buttons
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_off_Click(object sender, EventArgs e)
+        {
+            btn_off.Enabled = false;
+            btn_on.Enabled = true;
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            tb_fidget.Clear();
+        }
+
+        private void btn_rammstein_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
